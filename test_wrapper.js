56fcfd105b9e09d308dd8953bd9ef3ad0ef9d113
Dave Barker <kzar@kzar.co.uk>
2016-09-25 15:27:03 +0200

    Issue 4465 - Move rsa module from adblockpluschrome

--- test_wrapper.js
+++ test_wrapper.js
@@ -1,5 +1,9 @@
 "use strict";
 
+GLOBAL.atob = data => new Buffer(data, "base64").toString("binary");
+GLOBAL.btoa = data => new Buffer(data, "binary").toString("base64");
+GLOBAL.navigator = {};
+
 require("node-babel")();
 
 require("./node_modules/.bin/nodeunit");
