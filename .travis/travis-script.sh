081c269b39a6b693cffac09fa4cd5ea3977cec6f
Sergei Zabolotskikh <sergei@adblockplus.org>
2018-03-06 16:47:48 +0100

    Issue 6220 - include running of native tests on travis-ci for emscripten
    We are implementing some fundamental functionality and it's tested in native tests, so it makes a lot of sense to run those tests automatically.

--- .travis/travis-script.sh
+++ .travis/travis-script.sh
@@ -7,6 +7,14 @@ set -e
 
 PATH=${PATH}:${TRAVIS_BUILD_DIR}/third_party/ninja:$(python3 -c "import sysconfig;print(sysconfig.get_paths('posix_user')['scripts'])")
 
-meson --buildtype ${BUILDTYPE} build
-ninja -C build
+meson --buildtype ${BUILDTYPE} build/js
+ninja -C build/js
 npm test
+
+USE_ASAN=true
+if [[ "${BUILDTYPE}" == "release" ]]; then
+USE_ASAN=false
+fi 
+meson -Dnative=true -Dasan=${USE_ASAN} --buildtype ${BUILDTYPE} build/native
+ninja -C build/native
+./build/native/abptest
