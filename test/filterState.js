9c4dd99eb42dc41d997e233435d103c0ef5f7751
Hubert Figuière <h.figuiere@eyeo.com>
2020-09-15 17:26:17 -0400

    Refs #199 - Add tests for filterState.resetHitCount()

--- test/filterState.js
+++ test/filterState.js
@@ -1785,3 +1785,221 @@ describe("filterState.setHitCount()", function()
     });
   });
 });
+
+describe("filterState.resetHitCount()", function()
+{
+  let events = null;
+
+  function checkEvents(func, expectedEvents = [])
+  {
+    events = [];
+
+    func();
+
+    assert.deepEqual(events, expectedEvents);
+  }
+
+  beforeEach(function()
+  {
+    let sandboxedRequire = createSandbox();
+    (
+      {filterState} = sandboxedRequire("../lib/filterState"),
+      {filterNotifier} = sandboxedRequire("../lib/filterNotifier")
+    );
+
+    filterNotifier.on("filterState.hitCount", (...args) => events.push(args));
+  });
+
+  context("No state", function()
+  {
+    it("should not emit filterState.hitCount when filter's hit count is reset", function()
+    {
+      checkEvents(() => filterState.resetHitCount("||example.com^"));
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset from 1", function()
+    {
+      filterState.setHitCount("||example.com^", 1);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should not emit filterState.hitCount when filter's hit count is re-reset from 1", function()
+    {
+      filterState.setHitCount("||example.com^", 1);
+      filterState.resetHitCount("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"));
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset after filter hit is registered", function()
+    {
+      filterState.registerHit("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should not emit filterState.hitCount when filter's hit count is reset after filter hits are reset", function()
+    {
+      filterState.resetHits("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"));
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset from 1 after filter is disabled", function()
+    {
+      filterState.setEnabled("||example.com^", false);
+      filterState.setHitCount("||example.com^", 1);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset from 1 after filter's enabled state is reset", function()
+    {
+      filterState.resetEnabled("||example.com^");
+      filterState.setHitCount("||example.com^", 1);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset from 1 after filter's enabled state is toggled", function()
+    {
+      filterState.toggleEnabled("||example.com^");
+      filterState.setHitCount("||example.com^", 1);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset from 1 after filter's last hit time is set to 946684800000", function()
+    {
+      filterState.setLastHit("||example.com^", 946684800000);
+      filterState.setHitCount("||example.com^", 1);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset from 1 after filter's last hit time is reset", function()
+    {
+      filterState.resetLastHit("||example.com^");
+      filterState.setHitCount("||example.com^", 1);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset from 1 after filter state is reset", function()
+    {
+      filterState.reset("||example.com^");
+      filterState.setHitCount("||example.com^", 1);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset from 1 after filter state is serialized", function()
+    {
+      [...filterState.serialize("||example.com^")];
+      filterState.setHitCount("||example.com^", 1);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+  });
+
+  context("State: hitCount = 1", function()
+  {
+    beforeEach(function()
+    {
+      filterState.fromObject("||example.com^", {hitCount: 1});
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset", function()
+    {
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should not emit filterState.hitCount when filter's hit count is reset from 0", function()
+    {
+      filterState.setHitCount("||example.com^", 0);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"));
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset after filter hit is registered", function()
+    {
+      filterState.registerHit("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 2]]);
+    });
+
+    it("should not emit filterState.hitCount when filter's hit count is reset after filter hits are reset", function()
+    {
+      filterState.resetHits("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"));
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset after filter is disabled", function()
+    {
+      filterState.setEnabled("||example.com^", false);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset after filter's enabled state is reset", function()
+    {
+      filterState.resetEnabled("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset after filter's enabled state is toggled", function()
+    {
+      filterState.toggleEnabled("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset after filter's last hit time is set to 946684800000", function()
+    {
+      filterState.setLastHit("||example.com^", 946684800000);
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset after filter's last hit time is reset", function()
+    {
+      filterState.resetLastHit("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+
+    it("should not emit filterState.hitCount when filter's hit count is reset after filter state is reset", function()
+    {
+      filterState.reset("||example.com^");
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"));
+    });
+
+    it("should emit filterState.hitCount when filter's hit count is reset after filter state is serialized", function()
+    {
+      [...filterState.serialize("||example.com^")];
+
+      checkEvents(() => filterState.resetHitCount("||example.com^"),
+                  [["||example.com^", 0, 1]]);
+    });
+  });
+});
