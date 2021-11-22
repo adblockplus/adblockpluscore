62df873ff2440033d9238cce356e8f2bfcf6430e
Anton Smirnov <a.smirnov@eyeo.com>
2021-11-22 14:35:22 +0500

    Fix webext-sdk#127 - Create "declarative_net_request" part which should be included in Manifest file Add the script (including convert script draft - to use it's output directory). Cover with tests.

---
+++ scripts/generateSubscriptionsFragment.js
@@ -0,0 +1,74 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* eslint no-console: "off" */
+
+"use strict";
+
+const {
+  readdirSync, existsSync,
+  promises: {writeFile}
+} = require("fs");
+const path = require("path");
+const {outputDir: convertOutputDir} = require("./convertSubscriptions.js");
+
+const outputFile = "build/data/subscriptions/fragment.json";
+
+function generateFragment(dir, space = 2) {
+  if (!existsSync(dir)) {
+    throw new Error(`DNR rules directory (${dir}) does not exist. ` +
+      "Run `npm run \"convert-subscriptions\"` to generate it.");
+  }
+
+  let files = readdirSync(dir);
+  let fragment = {rule_resources: []};
+  for (let key in files) {
+    let file = files[key];
+    if (path.extname(file) !== ".json") {
+      console.warn(`Not .json file (${file}) skipped`);
+      continue;
+    }
+    fragment.rule_resources.push({
+      id: getRuleId(file),
+      enabled: false,
+      path: getFilePath(file)
+    });
+  }
+  if (fragment.rule_resources.length === 0)
+    console.warn("No static rules generated.");
+  return JSON.stringify(fragment, null, space);
+}
+
+function getRuleId(file) {
+  return path.parse(file).name; // without extension
+}
+
+function getFilePath(file) {
+  return path.parse(file).base;
+}
+
+async function main() {
+  let fragmentJson = generateFragment(convertOutputDir);
+  await writeFile(outputFile, fragmentJson, "utf8");
+  console.log(`Web extension manifest fragment file (${outputFile}) generated.`);
+}
+
+if (require.main == module)
+  main();
+
+exports.outputFile = outputFile;
+exports.generateFragment = generateFragment;
