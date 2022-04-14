9e1e227382f6ef33f6c58d31d52c9f101a2972b2
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2022-04-14 15:09:58 +0200

    Fetch artifacts properly

--- benchmark/extendHistoricalData.sh
+++ benchmark/extendHistoricalData.sh
@@ -1,19 +1,30 @@
   #!/bin/sh -e
   ref='issue-403'
   project='eyeo%2Fadblockplus%2Fabc%2Fadblockpluscore'
+  #current_pipeline_id=$(curl -sS -H "Content-Type: application/json" \
+   #                           'https://gitlab.com/api/v4/projects/'$project'/pipelines?per_page=200' | \
+   #                       jq -r \
+   #                          'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
   current_pipeline_id=$(curl -sS -H "Content-Type: application/json" \
-                              'https://gitlab.com/api/v4/projects/'$project'/pipelines' | \
-                          jq -r \
+                              'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/pipelines?per_page=100' | \
+                         jq -r \
                              'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
-  current_job_id=$(curl -sS -H "Content-Type: application/json" \
-                           'https://gitlab.com/api/v4/projects/'$project'/pipelines/'$current_pipeline_id'/jobs' | \
+  #current_job_id=$(curl -sS -H "Content-Type: application/json" \
+   #                        'https://gitlab.com/api/v4/projects/'$project'/pipelines/'$current_pipeline_id'/jobs' | \
+   #                  jq -r \
+   #                     'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
+echo 'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/pipelines/'$current_pipeline_id'/jobs' 
+   current_job_id=$(curl -sS -H "Content-Type: application/json" \
+                           'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/pipelines/'$current_pipeline_id'/jobs' | \
                      jq -r \
-                        'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
-  fetch_dir=$PWD/benchmark.fetched
+                        'map(select(.ref=="'$ref'" and .name=="benchmark")) | first | .id')
+  fetch_dir=$PWD/benchmark/
   test -d $fetch_dir || mkdir $fetch_dir
+  echo 'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/jobs/'$current_job_id'/artifacts'
   curl -sS -L \
        --output $fetch_dir/artifacts.zip \
-       'https://gitlab.com/api/v4/projects/'$project'/jobs/'$current_job_id'/artifacts'
-  # extract without subdirectory names, overwrite all
-  #7za e -aoa -o$output $fetch_dir/artifacts.zip
+      'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/jobs/'$current_job_id'/artifacts'
+    # 'https://gitlab.com/api/v4/projects/'$project'/jobs/'$current_job_id'/artifacts'
+
   unzip $fetch_dir/artifacts.zip
+  rm -rf artifacts.zip
