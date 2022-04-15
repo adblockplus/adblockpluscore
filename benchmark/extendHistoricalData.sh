cd1907b863b3e08a0bc0f9fe76dfd60128c57de9
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2022-04-15 11:42:11 +0200

    use project name as variable

--- benchmark/extendHistoricalData.sh
+++ benchmark/extendHistoricalData.sh
@@ -1,24 +1,27 @@
   #!/bin/sh -e
   ref='issue-403'
-  project='eyeo%2Fadblockplus%2Fabc%2Fadblockpluscore'
-  #current_pipeline_id=$(curl -sS -H "Content-Type: application/json" \
-   #                           'https://gitlab.com/api/v4/projects/'$project'/pipelines?per_page=200' | \
-   #                       jq -r \
-   #                          'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
+  #This should be switched to core once artifacts will be available on master
+  project='a.czyzewska'
+  # project='eyeo%2Fadblockplus%2Fabc'
+  echo 'https://gitlab.com/api/v4/projects/'$project'%2Fadblockpluscore/pipelines?per_page=200'
   current_pipeline_id=$(curl -sS -H "Content-Type: application/json" \
-                              'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/pipelines?per_page=100' | \
-                         jq -r \
+                              'https://gitlab.com/api/v4/projects/'$project'%2Fadblockpluscore/pipelines?per_page=200' | \
+                          jq -r \
                              'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
-  #current_job_id=$(curl -sS -H "Content-Type: application/json" \
-   #                        'https://gitlab.com/api/v4/projects/'$project'/pipelines/'$current_pipeline_id'/jobs' | \
-   #                  jq -r \
-   #                     'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
-echo 'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/pipelines/'$current_pipeline_id'/jobs' 
+  #current_pipeline_id=$(curl -sS -H "Content-Type: application/json" \
+   #                           'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/pipelines?per_page=100' | \
+   #                      jq -r \
+    #                         'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
+  current_job_id=$(curl -sS -H "Content-Type: application/json" \
+                           'https://gitlab.com/api/v4/projects/'$project'%2Fadblockpluscore/pipelines/'$current_pipeline_id'/jobs' | \
+                     jq -r \
+                       'map(select(.ref=="'$ref'" and .status=="success")) | first | .id')
+#echo 'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/pipelines/'$current_pipeline_id'/jobs' 
    current_job_id=$(curl -sS -H "Content-Type: application/json" \
-                           'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/pipelines/'$current_pipeline_id'/jobs' | \
+                           'https://gitlab.com/api/v4/projects/'$project'%2Fadblockpluscore/pipelines/'$current_pipeline_id'/jobs' | \
                      jq -r \
                         'map(select(.ref=="'$ref'" and .name=="benchmark")) | first | .id')
-  fetch_dir=$PWD/benchmark/
+  fetch_dir=$PWD/benchmark
   test -d $fetch_dir || mkdir $fetch_dir
   echo 'https://gitlab.com/api/v4/projects/a.czyzewska%2Fadblockpluscore/jobs/'$current_job_id'/artifacts'
   curl -sS -L \
