988138f0e837fd6281d4919f801255a21f6fc7bb
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2022-04-21 12:14:09 +0200

    remove echo fix failures

--- benchmark/benchmark-entrypoint.sh
+++ benchmark/benchmark-entrypoint.sh
@@ -6,13 +6,12 @@ npm install
 #npm run benchmark:save
 CURRENTTS=$(date +%FT%TZ)
 FLAGS="--save --save-temp --ts=$CURRENTTS";
-echo $FLAGS
-npm run benchmark:easylist -- $FLAGS && npm run benchmark:easylist+AA --$FLAGS && npm run benchmark:allFilters -- $FLAGS && npm run benchmark:match:all -- $FLAGS && npm run benchmark:match:all:easylist -- $FLAGS && npm run benchmark:match:all:easylist+AA -- $FLAGS && npm run benchmark:match:all:allFilters -- $FLAGS
+npm run benchmark:easylist -- $FLAGS && npm run benchmark:easylist+AA --$FLAGS && npm run benchmark:allFilters -- $FLAGS && npm run benchmark:match:all -- $FLAGS && npm run benchmark:match:all:easylist -- $FLAGS && npm run benchmark:match:all:easylist+AA -- $FLAGS && npm run benchmark:match:all:allFilters -- $FLAGS --dt
 #git checkout origin master
 npm install
 REFSTS=$(date +%FT%TZ)
 FLAGS="--save --save-temp --ts=$REFSTS";
-npm run benchmark:easylist --$FLAGS && npm run benchmark:easylist+AA --$FLAGS && npm run benchmark:allFilters -- $FLAGS && npm run benchmark:match:all -- $FLAGS && npm run benchmark:match:all:easylist -- $FLAGS && npm run benchmark:match:all:easylist+AA -- $FLAGS && npm run benchmark:match:all:allFilters -- $FLAGS
+npm run benchmark:easylist --$FLAGS && npm run benchmark:easylist+AA --$FLAGS && npm run benchmark:allFilters -- $FLAGS && npm run benchmark:match:all -- $FLAGS && npm run benchmark:match:all:easylist -- $FLAGS && npm run benchmark:match:all:easylist+AA -- $FLAGS && npm run benchmark:match:all:allFilters -- $FLAGS --dt
 npm  --current=$CURRENTTS --refs=$REFSTS run test benchmark/compare-results.js
 
 if [[ "$EXTENDHISTORICAL" == true ]]; then
